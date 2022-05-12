c846c2d2ef9cac5eef5b4ce51f41af17759ecceb
naresh-500apps <naresh.yerramsetty@mantragroup.com>
2022-05-12 11:21:29 +0530

    commit

---
+++ tasks/src/components/11-05-2022/stringg.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <button @click="f1()">click</button>
+      It is a {{res}}
+      <button @click="f()">click1</button>
+      It is a {{res1}}
+  </div>
+</template>
+
+<script>
+export default {
+    name:"QN1",
+    data(){
+        return{
+            s1:"naresh",
+            s2:46544554,
+            res:"",
+            res1:""
+        }
+    },
+    methods:{
+        f1(){
+            this.res=typeof this.s1
+        },
+        f(){
+            this.res1=typeof this.s2
+        }
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>

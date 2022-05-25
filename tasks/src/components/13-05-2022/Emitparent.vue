3afa8de6b9a0b120004421cb626316c0f53ecad9
naresh-500apps <naresh.yerramsetty@mantragroup.com>
2022-05-25 20:26:23 +0530

    commit

---
+++ tasks/src/components/13-05-2022/Emitparent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <Emitchild @action="fun1" ></Emitchild>
+      <p>{{ name }} </p>
+  </div>
+</template>
+
+<script>
+import Emitchild from './Emitchild.vue'
+export default {
+    name:"EmitParent",
+    components:{
+        Emitchild
+    },
+    data(){
+        return{
+           name:"your name is:"
+        }
+    },
+    methods:{
+        fun1(change){
+            this.name = change.name;
+        },
+    }
+
+}
+</script>
+
+<style>
+
+</style>
